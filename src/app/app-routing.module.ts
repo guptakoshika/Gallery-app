ada20a22827a28cf6a8fa41a4a72c11148ca2298
Koshika Gupta <koshikaje@gmail.com>
2018-11-11 16:54:20 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { DetailpageComponent } from './detailpage/detailpage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "gallery", component:HeaderComponent },
+  {path: "images/:id",component: DetailpageComponent},
+  {path:"**",component:ErrorpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
