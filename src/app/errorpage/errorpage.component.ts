cbc2654dfb574b0a966852f8e13d9b91b6a9717b
Koshika Gupta <koshikaje@gmail.com>
2018-11-19 18:02:50 +0530

    Add files via upload

---
+++ src/app/errorpage/errorpage.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-errorpage',
+  templateUrl: './errorpage.component.html',
+  styleUrls: ['./errorpage.component.css']
+})
+export class ErrorpageComponent  {
+
+ 
+ 
+  constructor()
+  { 
+    
+    console.log(" IK am in error page")
+  }
+  
+  
+}
