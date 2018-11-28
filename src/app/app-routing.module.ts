96644d466da79cb8949465bfe812dcbd01c7e796
Koshika Gupta <koshikaje@gmail.com>
2018-11-28 20:40:26 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { DetailpageComponent } from './detailpage/detailpage.component';
+import { PicturesComponent } from './pictures/pictures.component';
 
 
 const routes: Routes = [
   
-  { path: 'gallery', component:HeaderComponent },
+  { path: 'gallery', component:PicturesComponent },
   {path:'' , redirectTo:'/gallery',pathMatch:'full'},
+  {path:'gallery/:filter' , component:HeaderComponent},
   {path: 'image/:id',component: DetailpageComponent},
   {path:'**',component:ErrorpageComponent}
 ];
