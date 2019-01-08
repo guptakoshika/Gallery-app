3f2f829948574b6640fea6718654d758213368fe
Koshika Gupta <koshikaje@gmail.com>
2019-01-08 22:43:33 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,21 @@ import { HeaderComponent } from './header/header.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { DetailpageComponent } from './detailpage/detailpage.component';
 import { PicturesComponent } from './pictures/pictures.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
-  
-  { path: 'gallery', component:PicturesComponent },
-  {path:'' , redirectTo:'/gallery',pathMatch:'full'},
-  {path:'gallery/:filter' , component:HeaderComponent},
-  {path: 'image/:id',component: DetailpageComponent},
-  {path:'**',component:ErrorpageComponent}
+  {path:'signup' ,component:SignupComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'forgotpassword' ,component:ForgotComponent},
+  {path:'' , redirectTo:'/login',pathMatch:'full'},
+  { path: 'gallery', component:PicturesComponent, canActivate:[AuthGuard]},
+  {path:'gallery/:filter' , component:HeaderComponent, canActivate:[AuthGuard]},
+  {path: 'image/:id',component: DetailpageComponent, canActivate:[AuthGuard]},
+  {path:'**',component:ErrorpageComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
